c46af75f90c45a6557038ddfb904528170567528
SeonHyungJo <seonhyung.jo@gmail.com>
2023-06-11 02:46:48 +0900

    sitemap.xml robots.txt 추가

---
+++ sitemap.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'blog.snyung.com',
+  output: 'export',
+  generateRobotsTxt: true,
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 7000,
+  autoLastmod: true,
+  outDir: 'out',
+  exclude: [],
+}
