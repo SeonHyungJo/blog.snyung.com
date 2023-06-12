dd215ee99d730fbb2f2ae4c23eba3176b32c9c51
SeonHyungJo <seonhyung.jo@gmail.com>
2023-06-12 17:18:56 +0900

    상단 메뉴 컴포넌트 정리

--- app/@components/Header.tsx
+++ app/@components/Header.tsx
@@ -5,88 +5,55 @@ import { useSelectedLayoutSegments } from 'next/navigation';
 import Link from 'next/link';
 import Image from 'next/image';
 import cx from 'classnames';
-import { BottomSheet, BottomSheetRef } from 'react-spring-bottom-sheet'
+import { BottomSheet } from 'react-spring-bottom-sheet'
+
+import { TOP_MENU } from '@/blog.config'
 
 import 'react-spring-bottom-sheet/dist/style.css'
 
 
-const menuData: { name: string, url: string }[] = [
-  {
-    name: 'Posts',
-    url: '/posts'
-  },
-  {
-    name: 'Series',
-    url: '/series'
-  },
-  {
-    name: 'Snippets',
-    url: '/snippets'
-  },
-  {
-    name: 'Archives',
-    url: '/archives'
-  },
-]
-
 export default function Header() {
+  return (
+    <header>
+      <Navigation/>
+      <MobileNavigation />
+    </header>
+  )
+}
+
+function Navigation() {
   const [firtSegment = ''] = useSelectedLayoutSegments()
 
   return (
-    <>
-      <header>
-        <section className='flex flex-row justify-between py-10 px-6'>
-          <section className='flex flex-row justify-start items-center gap-3'>
-            <Link prefetch href={'/'}>
-              <Image
-                width={36}
-                height={36}
-                src={'/images/common/logo.png'}
-                alt='logo'
-                style={{ cursor: 'pointer', width: 36, height: 36 }}
-              />
-            </Link>
+    <section className='flex flex-row justify-between py-10 px-6'>
+      <section className='flex flex-row justify-start items-center gap-3'>
+        <section className={'hover:bg-slate-100 transition-all py-2 px-3 rounded-lg'}>
+          <Link prefetch href={'/'}>
+            <Image
+              width={36}
+              height={36}
+              src={'/images/common/logo.png'}
+              alt='logo'
 
-            <section className={'flex flex-row justify-start items-center max-sm:hidden'}>
-              {menuData.map(({ name, url }) => (
-                <Link
-                  key={url}
-                  href={url}
-                  prefetch
-                  className={cx('text-base text-slate-600 hover:bg-slate-100 transition-all py-2 px-3 rounded-lg', url === `/${firtSegment}` && 'font-bold')}
-                >
-                  {name}
-                </Link>
-              ))}
-            </section>
-          </section>
-
-          {/* <section className='flex flex-row justify-start items-center gap-3'>
-            <span>
-              {'검색창'}
-            </span>
-            <section className={'flex flex-row justify-center items-center'}>
-              <Image
-                width={24}
-                height={24}
-                src={'/images/common/theme-light.png'}
-                alt='logo'
-                style={{ cursor: 'pointer', width: 24, height: 24 }}
-              />
-              <Image
-                width={24}
-                height={24}
-                src={'/images/common/theme-dark.png'}
-                alt='logo'
-                style={{ cursor: 'pointer', width: 24, height: 24 }}
-              />
-            </section>
-          </section> */}
+              style={{ cursor: 'pointer', width: 36, height: 36, }}
+            />
+          </Link>
         </section>
-      </header>
 
-      <MobileNavigation />
-    </>
+        <section className={'flex flex-row justify-start items-center max-sm:hidden'}>
+          {TOP_MENU.map(({ name, url }) => (
+            <Link
+              key={url}
+              href={url}
+              prefetch
+              className={cx('text-base text-slate-600 hover:bg-slate-100 transition-all py-2 px-3 rounded-lg', url === `/${firtSegment}` && 'font-bold')}
+            >
+              {name}
+            </Link>
+          ))}
+        </section>
+      </section>
+    </section>
   )
 }
 
@@ -107,7 +74,7 @@ function MobileNavigation() {
 
       <BottomSheet open={openMenu} onDismiss={() => setOpenMenu(false)}>
         <section className='flex flex-col justify-start items-center gap-3 p-10'>
-          {menuData.map(({ name, url }) => (
+          {TOP_MENU.map(({ name, url }) => (
             <Link
               key={url}
               href={url}
@@ -120,6 +87,32 @@ function MobileNavigation() {
           ))}
         </section>
       </BottomSheet>
+    </section>
+  )
+}
+
+function SearchAndThemeChanger() {
+  return (
+    <section className='flex flex-row justify-start items-center gap-3'>
+      <span>
+        {'검색창'}
+      </span>
+      <section className={'flex flex-row justify-center items-center'}>
+        <Image
+          width={24}
+          height={24}
+          src={'/images/common/theme-light.png'}
+          alt='logo'
+          style={{ cursor: 'pointer', width: 24, height: 24 }}
+        />
+        <Image
+          width={24}
+          height={24}
+          src={'/images/common/theme-dark.png'}
+          alt='logo'
+          style={{ cursor: 'pointer', width: 24, height: 24 }}
+        />
+      </section>
     </section>
   )
 }
---
+++ blog.config.ts
@@ -0,0 +1,23 @@
+type MENU = {
+  name: string;
+  url: string;
+}
+
+export const TOP_MENU: MENU[] = [
+  {
+    name: 'Posts',
+    url: '/posts'
+  },
+  {
+    name: 'Series',
+    url: '/series'
+  },
+  {
+    name: 'Snippets',
+    url: '/snippets'
+  },
+  {
+    name: 'Archives',
+    url: '/archives'
+  },
+]
