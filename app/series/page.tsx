274a2b0971f6e284d3fdd59e8304f8d2f1749d1b
SeonHyungJo <seonhyung.jo@gmail.com>
2023-06-13 01:11:51 +0900

    Posts를 제외한 카테고리 예정으로 변경

--- app/series/page.tsx
+++ app/series/page.tsx
@@ -3,7 +3,7 @@ import TopSection from "../@components/TopSection";
 export default async function SeriesMainPage() {
   return (
     <TopSection
-      title={'Series'}
+      title={'(예정) Series'}
       content={'📚 시리즈로 작성된 글을 모아놓은 공간입니다.'}
     />
   );
--- app/snippets/page.tsx
+++ app/snippets/page.tsx
@@ -3,7 +3,7 @@ import TopSection from "../@components/TopSection"
 export default async function SnippetsMainPage() {
   return (
     <TopSection
-      title={'Snippets'}
+      title={'(예정) Snippets'}
       content={'⚡️ 유용한 정보를 모아놓은 공간입니다.'}
     />
   );
