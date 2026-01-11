9e15930c1cfc8e53155f4916115d531d34a6b6c0
SeonHyungJo <seonhyung.jo@gmail.com>
2026-01-11 12:37:16 +0900

    refactor: logo 사이즈 수정

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -7,17 +7,14 @@ export default function Header() {
   return (
     <header className="pt-safe">
       <section className="sticky top-0 flex flex-row justify-center pt-2 w-full sm:pt-6">
-        <section className={"py-2 px-3 rounded-lg"}>
-          <Link href={"/"}>
-            <ExportedImage
-              width={120}
-              height={36}
-              src={"/images/common/snyung.png"}
-              alt="logo"
-              style={{ cursor: "pointer", width: 120, height: 36 }}
-            />
-          </Link>
-        </section>
+        <Link href={"/"} style={{ cursor: "pointer", width: 160, height: 48 }}>
+          <ExportedImage
+            width={160}
+            height={48}
+            src={"/images/common/snyung.png"}
+            alt="logo"
+          />
+        </Link>
       </section>
     </header>
   );
